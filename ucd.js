bb512348a2b7358428de5959f4e6e70ff9ab6ce7
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2017-03-18 18:18:59 -0400

    load and dump cache

--- ucd.js
+++ ucd.js
@@ -4,11 +4,11 @@ const Fuse = require('fuse.js')
 const path = require('path')
 
 class UnicodeDatabase {
-  constructor(maxHits = 100) {
+  constructor(ucdFilename, maxHits = 100) {
     this.maxHits = maxHits
     this.database = []
     this.lookup = {}
-    this.loadDatabase(path.join(__dirname, 'ucd.nounihan.simplified.json'))
+    this.loadDatabase(ucdFilename)
   }
 
   loadDatabase(filename) {
@@ -47,12 +47,30 @@ class UnicodeDatabase {
 }
 
 class CachedUnicodeDatabase {
-  constructor(aliases = {}, cacheSize = 1000, maxHits = 50) {
+  constructor(ucdFilename, cacheFilename, aliases = {}, cacheSize = 1000, maxHits = 50) {
     this.cache = {}
     this.history = []
     this.cacheSize = cacheSize
     this.aliases = {}
-    this.ucd = new UnicodeDatabase(maxHits)
+    this.ucd = new UnicodeDatabase(ucdFilename, maxHits)
+    this.load(cacheFilename)
+  }
+
+  load(filename) {
+    try {
+      const data = JSON.parse(fs.readFileSync(filename, "utf8"))
+      if (data && data['cache'] && data['history']) {
+        this.cache = data['cache']
+        this.history = data['history']
+      }
+    } catch (err) {
+      // do nothing
+    }
+  }
+
+  dump(filename) {
+    const cache = JSON.stringify({cache: this.cache, history: this.history})
+    fs.writeFileSync(filename, cache)
   }
 
   addAlias(alias, value) {
@@ -77,8 +95,11 @@ class CachedUnicodeDatabase {
     return result
   }
 }
-ucd = new CachedUnicodeDatabase()
 
+ucd = new CachedUnicodeDatabase(
+  path.join(__dirname, 'ucd.nounihan.simplified.json'),
+  path.join(__dirname, 'cache.json')
+)
 
 { // TODO: this has to be done transparently
   const greekAlphabets = ['alpha', 'beta', 'gamma', 'delta']
@@ -94,4 +115,5 @@ ucd = new CachedUnicodeDatabase()
     ucd.addAlias(gaUpper, queryUpper)
     ucd.search(gaUpper)
   }
+  ucd.dump(path.join(__dirname, 'cache.json'))
 }
