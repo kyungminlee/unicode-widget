98f1850adbcd8da399b6bf09fe771f966c2eeeee
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-10-04 16:11:38 -0400

    Add menu to toggle always on top

--- main.js
+++ main.js
@@ -1,46 +1,74 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const { Menu, MenuItem } = require('electron');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
-let mainWindow
+let mainWindow;
+
+// function setAlwaysOnTop(value) {
+//   //alert("Hi");
+//   mainWindow.alwaysOnTop = value;
+// }
+
 
 function createWindow () {
   mainWindow = new BrowserWindow({
-      width: 400,
-      height: 600, 
-      alwaysOnTop: true, 
-      frame: true,
-      icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
-      webPreferences: {
-          nodeIntegration: true
-      }
-  })
+    width: 400,
+    height: 600, 
+    alwaysOnTop: true, 
+    frame: true,
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   mainWindow.on('closed', function () {
     mainWindow = null
-  })
+  });
+
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Always On Top',
+          click(menuItem, browserWindow, event) { 
+            mainWindow.setAlwaysOnTop(menuItem.checked);
+          },
+          type: "checkbox",
+          checked: true
+        },
+        {
+          label: 'Exit',
+          click() { app.quit(); }
+        }
+      ]
+    }
+  ])
+  Menu.setApplicationMenu(menu);
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
 app.on('activate', function () {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
